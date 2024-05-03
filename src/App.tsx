37676c694a59ce897761a5fcb1d5c3385bb32155
dlsxjzld <sinjuk1@naver.com>
2024-05-03 20:18:37 +0900

    [✨feat]: 1rm 계산 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,56 @@
-import Demo from '@/Demo';
+import { ChangeEvent, useState } from 'react';
 
 function App() {
+  const [oneRm, setOneRm] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [repeat, setRepeat] = useState(0);
+
+  // 선택값 : 횟수
+  // 입력값 : 무게
+  const REPEAT_SELECT_LIST = Array.from(
+    { length: 12 },
+    (_, index) => index + 1
+  );
+
+  const calculate1RM = (weight: number, repeat: number) => {
+    return weight * (36 / (37 - repeat));
+  };
+
+  const getWeight = (e: ChangeEvent<HTMLInputElement>) => {
+    setWeight(Number(e.target.value));
+  };
+
+  const handleSelect = (e: ChangeEvent<HTMLSelectElement>) => {
+    setRepeat(Number(e.target.value));
+  };
+
+  const handleCalculate = () => {
+    const oneRm = calculate1RM(weight, repeat);
+    setOneRm(oneRm);
+  };
+
   return (
     <>
-      <Demo></Demo>
+      <input
+        placeholder='무게'
+        type='number'
+        name='무게'
+        onChange={getWeight}
+      />
+      <select
+        name='횟수'
+        onChange={handleSelect}>
+        {REPEAT_SELECT_LIST.map((value) => (
+          <option key={value}>{value}</option>
+        ))}
+      </select>
+      <button
+        type='button'
+        onClick={handleCalculate}>
+        click!
+      </button>
+      <div>{`weight: ${weight} repeat: ${repeat}`}</div>
+      <div>{`1rm : ${oneRm}`}</div>
     </>
   );
 }
