d16e3268ada9395976ddd7c173091e9294eee044
dlsxjzld <sinjuk1@naver.com>
2024-05-08 22:22:44 +0900

    [🎨style]: 기본적인 디자인 추가

--- src/App.tsx
+++ src/App.tsx
@@ -38,42 +38,50 @@ function App() {
 
   const handleReset = () => {
     setWeight('');
-    setRepeat(0);
+    setRepeat(1);
     setResult([]);
   };
 
   return (
-    <>
+    <div className='min-w-[37.5rem] min-h-full flex flex-col justify-center items-center p-[2rem_1.5rem]'>
+      <h1 className='font-bold text-[2rem] pb-[2rem]'>1RM 계산기</h1>
       <input
         placeholder='무게'
         type='number'
         name='무게'
         onChange={getWeight}
         value={weight}
+        className='w-full border-[0.2rem]'
       />
       <select
         name='횟수'
+        className='w-full border-[0.2rem]'
         onChange={handleSelect}>
         {REPEAT_SELECT_LIST.map((value) => (
           <option key={value}>{value}</option>
         ))}
       </select>
-      <button
-        type='button'
-        className='mr-2'
-        onClick={handleCalculate}>
-        계산
-      </button>
-      <button
-        type='button'
-        onClick={handleReset}>
-        초기화
-      </button>
-      <div>{`weight: ${weight} repeat: ${repeat}`}</div>
-      {result.map((weight, rep) => (
-        <div>{`${rep + 1}RM : ${weight}kg`}</div>
-      ))}
-    </>
+      <section className='w-full flex flex-row justify-around'>
+        <button
+          type='button'
+          className='w-1/2 border-[0.2rem]'
+          onClick={handleCalculate}>
+          계산
+        </button>
+        <button
+          type='button'
+          className='w-1/2 border-[0.2rem]'
+          onClick={handleReset}>
+          초기화
+        </button>
+      </section>
+      <section className='w-full flex flex-col items-center pt-[2rem] text-[1.8rem] text-center gap-[0.5rem]'>
+        <span className='w-full font-semibold text-center'>{`무게: ${weight || 0} 횟수: ${repeat}`}</span>
+        {result.map((weight, rep) => (
+          <span className='w-full text-[1.6rem]'>{`${rep + 1}RM : ${weight}kg`}</span>
+        ))}
+      </section>
+    </div>
   );
 }
 
