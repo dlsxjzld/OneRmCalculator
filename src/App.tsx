29ec6e4bd00f695104c7359bc07af305d375b9fb
dlsxjzld <sinjuk1@naver.com>
2024-05-22 18:34:10 +0900

    [♻️refactor]: 유효성 검사 관련 함수들 전부 util/validation.ts 로 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
 import { ChangeEvent, Fragment, useState } from 'react';
+import {
+  validateRepeatExist,
+  validateWeightDecimalPointLength,
+  validateWeightExist,
+  validateWeightRange,
+} from './util/validation';
 
 const REPS = 12;
 
@@ -26,33 +32,6 @@ const App = () => {
     { length: REPS },
     (_, index) => index + 1
   );
-
-  const validateWeightRange = (weight: string): boolean => {
-    const _weightCheckForRange = Number(weight);
-    if (_weightCheckForRange < 0 || _weightCheckForRange > 1000) {
-      alert('입력 가능한 범위는 0~1000 입니다!');
-      return false;
-    }
-    return true;
-  };
-
-  const validateWeightDecimalPointLength = (weight: string): boolean => {
-    const _weightCheckForLength = weight.split('.')[1]?.length;
-
-    if (_weightCheckForLength && _weightCheckForLength > 2) {
-      alert('정확도를 위해 소수점 2자리까지만 입력해 주세요!');
-      return false;
-    }
-    return true;
-  };
-
-  const validateWeightExist = (weight: string) => {
-    return weight !== '';
-  };
-
-  const validateRepeatExist = (repeat: number) => {
-    return repeat !== 0;
-  };
 
   const handleWeight = (e: ChangeEvent<HTMLInputElement>) => {
     if (
