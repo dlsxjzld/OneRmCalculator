a7a544898a0150d7c828f6e5c81c92d160f09cb5
dlsxjzld <sinjuk1@naver.com>
2024-05-22 21:04:57 +0900

    [🎨style]: dark mode 스타일 완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, Fragment, useState } from 'react';
+import DarkModeIcon from './DarkModeIcon';
 import {
   validateRepeatExist,
   validateWeightDecimalPointLength,
@@ -31,6 +32,7 @@ const App = () => {
   const [weight, setWeight] = useState('');
   const [repeat, setRepeat] = useState(0);
   const [result, setResult] = useState<string[]>([]);
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   const handleWeight = (e: ChangeEvent<HTMLInputElement>) => {
     if (
@@ -73,12 +75,27 @@ const App = () => {
     setResult([]);
   };
 
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+    if (!isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  };
+
   return (
-    <div className='w-screen h-screen bg-blue-500 py-[1.6rem] overflow-auto scroll-smooth'>
-      <div className='max-w-[33.6rem] min-h-[32rem] mx-auto bg-white rounded-[0.5rem] shadow-md p-[2rem_1.6rem]'>
-        <header className='text-center mb-[1rem]'>
-          <h1 className='font-bold text-[2rem]'>1RM 계산기</h1>
-          <h2 className='font-medium'>
+    <div
+      className={`w-screen h-screen bg-blue-500 py-[1.6rem] overflow-auto scroll-smooth dark:bg-black/70 transition-all duration-300`}>
+      <div className='max-w-[33.6rem] min-h-[32rem] mx-auto bg-white rounded-[0.5rem] shadow-md p-[2rem_1.6rem] dark:bg-black'>
+        <header className='text-center mb-[1rem] relative'>
+          <button
+            onClick={toggleDarkMode}
+            className='absolute top-0 right-0 p-2 rounded-full hover:bg-gray-300 dark:hover:bg-gray-500 transition-all duration-300 text-gray-800'>
+            <DarkModeIcon />
+          </button>
+          <h1 className='font-bold text-[2rem] dark:text-white'>1RM 계산기</h1>
+          <h2 className='font-medium dark:text-white'>
             정확도를 위해 횟수를 10개 이하로 해주세요!
           </h2>
         </header>
@@ -89,10 +106,14 @@ const App = () => {
             name='무게'
             onChange={handleWeight}
             value={weight}
-            className='w-full p-[0.8rem_0.6rem] placeholder:text-black bg-customWhite-100 border rounded-[0.5rem] focus:outline-none focus:ring-2 focus:ring-blue-500'
+            className='w-full p-[0.8rem_0.6rem] placeholder:text-black dark:text-white bg-customWhite-100 border rounded-[0.5rem] focus:outline-none focus:ring-2 focus:ring-blue-500'
           />
-          <article className='w-full flex items-center justify-between'>
-            <label htmlFor='reps'>반복 횟수 :</label>
+          <article className='w-full flex items-center justify-between '>
+            <label
+              htmlFor='reps'
+              className='dark:text-white'>
+              반복 횟수 :
+            </label>
             <select
               id='reps'
               name='횟수'
@@ -116,19 +137,19 @@ const App = () => {
           <article className='w-full flex flex-row gap-[1.6rem]'>
             <button
               type='button'
-              className='w-1/2 p-[0.8rem_0.7rem] border bg-blue-500 text-white rounded-[0.5rem] hover:bg-customBlue-550 transition duration-300 '
+              className='w-1/2 p-[0.8rem_0.7rem] border bg-blue-500 dark:bg-gray-400 dark:text-black dark:border-black dark:hover:bg-gray-500 text-white rounded-[0.5rem] hover:bg-light-customBlue-550 transition-all duration-300'
               onClick={handleCalculate}>
               계산
             </button>
             <button
               type='button'
-              className='w-1/2 p-[0.8rem_0.7rem] border bg-customWhite-100  rounded-[0.5rem] hover:bg-customBlue-550 hover:text-white transition duration-300 '
+              className='w-1/2 p-[0.8rem_0.7rem] border bg-customWhite-100  rounded-[0.5rem] dark:hover:bg-gray-500 dark:border-black hover:bg-light-customBlue-550 hover:text-white transition-all duration-300 '
               onClick={handleReset}>
               초기화
             </button>
           </article>
-          <article className='w-full flex flex-col items-center pt-[1rem] text-[1.8rem] text-center gap-[0.5rem]'>
-            <div className='w-full flex justify-evenly font-semibold'>
+          <article className='w-full flex flex-col items-center pt-[1rem] text-[1.8rem] text-center gap-[0.5rem] dark:text-white'>
+            <div className='w-full flex justify-evenly font-semibold '>
               <p>{`무게: ${weight || 0}kg`}</p>
               <p>{`횟수: ${repeat}번`}</p>
             </div>
