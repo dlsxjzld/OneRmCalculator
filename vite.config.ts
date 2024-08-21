4c157eb45456bce15ed91264ac40a3d213c971e1
dlsxjzld <sinjuk1@naver.com>
2024-08-21 14:44:06 +0900

    [🔨chore]: vite-plugin-prerender 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import path from "path"
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+import prerender from 'vite-plugin-prerender';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    prerender({
+      staticDir: path.resolve(__dirname, 'dist'),
+      routes: ['/'],
+    }),
+  ],
   resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
-})
+});
